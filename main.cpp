c59253ca8e5ad086a5bdb3d94905f736c56c4d63
S5603732 <118397147+S5603732@users.noreply.github.com>
2022-12-08 12:54:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+
+using namespace std;
 
 int main(){
+   char b=0;
+   cin >> b;
+   char c=b-32;
+   char d=b+32;
+   if (lettere(b)==true) {
+       cout << "vero" << endl;
+       if(b>='a') cout << c ;
+       else cout << d;
+   }else cout << "falso";
   return 0;
 }
